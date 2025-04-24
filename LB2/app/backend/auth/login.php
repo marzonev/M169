badc46a4f6a7c2e97c44c631f776dba77ddb95bd
ubuntu <ubuntu@edu.tbz.ch>
2025-04-24 06:59:58 +0000

    removed nginx

--- LB2/app/backend/auth/login.php
+++ LB2/app/backend/auth/login.php
@@ -16,7 +16,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if ($user && password_verify($password, $user['password'])) {
         $_SESSION['user_id'] = $user['id'];
         $_SESSION['username'] = $user['username'];
-        header("Location: /backend/php/ticket/index.php");
+        header("Location: ../ticket/index.php");
         exit;
     } else {
         $error = "Login fehlgeschlagen.";
